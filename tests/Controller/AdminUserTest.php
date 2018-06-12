7313dd3b2a6a4a94dcfaf03016754c62e23e7899
Purvesh <ind.purvesh@gmail.com>
2018-06-13 10:45:27 +1200

    updated unit test

--- tests/Controller/AdminUserTest.php
+++ tests/Controller/AdminUserTest.php
@@ -3,6 +3,7 @@
 namespace AvoRed\Ecommerce\Tests\Controller;
 
 use AvoRed\Ecommerce\Tests\BaseTestCase;
+use AvoRed\Ecommerce\Models\Database\AdminUser;
 
 class AdminUserTest extends BaseTestCase
 {
@@ -11,38 +12,18 @@ class AdminUserTest extends BaseTestCase
      *
      * @return void
      */
-    public function testAdminUserIndexRouteTest()
+    public function testAdminUserRosourceRouteTest()
     {
         $user = $this->_getAdminUser();
         $response = $this->actingAs($user, 'admin')->get(route('admin.admin-user.index'));
         $response->assertStatus(200);
-        $response->assertSee('Purvesh');
-        $response->assertSee('Patel');
-    }
+        $response->assertSee('Admin User List');
 
-    /**
-     * Test to check if admin user create route is working
-     *
-     * @return void
-     */
-    public function testAdminUserCreateGetRouteTest()
-    {
-        $user = $this->_getAdminUser();
-        $response = $this->actingAs($user, 'admin')->get(route('admin.admin-user.create'));
+        $response = $this->get(route('admin.admin-user.create'));
         $response->assertStatus(200);
         $response->assertSee('Create Admin User');
-    }
 
-    /**
-     * Test to check if admin user create route is working
-     *
-     * @return void
-     */
-    public function testAdminUserCreatePostRouteTest()
-    {
-        $user = $this->_getAdminUser();
         $response = $this
-                        ->actingAs($user, 'admin')
                         ->post(
                             route('admin.admin-user.store'),
                                         [
@@ -54,6 +35,20 @@ class AdminUserTest extends BaseTestCase
                                             'role_id' => $user->role_id
                                         ]
                         );
+        $response->assertRedirect(route('admin.admin-user.index'));
+
+        $adminTestUser = AdminUser::whereEmail('admin@test.com')->first();
+
+        $response = $this->put(
+                            route('admin.admin-user.update', $adminTestUser->id),
+                            [
+                                'first_name' => 'test new name',
+                                'last_name' => 'test',
+                                'email' => 'admin@test.com',
+                                'role_id' => $user->role_id
+                            ]
+                        );
+
         $response->assertRedirect(route('admin.admin-user.index'));
     }
 }
