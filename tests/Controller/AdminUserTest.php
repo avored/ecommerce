f9d9988c94916ba2b81bf6ea0dae6cd793248599
Purvesh <ind.purvesh@gmail.com>
2018-06-13 08:52:33 +1200

    fixed some form attribute array_merge and added unit test

--- tests/Controller/AdminUserTest.php
+++ tests/Controller/AdminUserTest.php
@@ -21,15 +21,39 @@ class AdminUserTest extends BaseTestCase
     }
 
     /**
-    * Test to check if admin user create route is working
-    *
-    * @return void
-    */
-    public function testAdminUserCreateRouteTest()
+     * Test to check if admin user create route is working
+     *
+     * @return void
+     */
+    public function testAdminUserCreateGetRouteTest()
     {
         $user = $this->_getAdminUser();
         $response = $this->actingAs($user, 'admin')->get(route('admin.admin-user.create'));
         $response->assertStatus(200);
         $response->assertSee('Create Admin User');
+    }
+
+    /**
+     * Test to check if admin user create route is working
+     *
+     * @return void
+     */
+    public function testAdminUserCreatePostRouteTest()
+    {
+        $user = $this->_getAdminUser();
+        $response = $this
+                        ->actingAs($user, 'admin')
+                        ->post(
+                            route('admin.admin-user.store'),
+                                        [
+                                            'first_name' => 'test',
+                                            'last_name' => 'test',
+                                            'email' => 'admin@test.com',
+                                            'password' => 'admin123',
+                                            'password_confirmation' => 'admin123',
+                                            'role_id' => $user->role_id
+                                        ]
+                        );
+        $response->assertRedirect(route('admin.admin-user.index'));
     }
 }
